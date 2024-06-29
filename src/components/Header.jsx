a7350929b2cfdb79cf07aa817fe8e30f014d4b44
Edward1217 <m932012@gmail.com>
2024-06-28 18:26:04 -0700

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <Header>
+        <h1>
+            <span>Edward</span>
+            <span>Real Estate</span>
+        </h1>
+    </Header>
+  )
+}
