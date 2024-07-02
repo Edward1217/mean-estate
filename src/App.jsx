ac000239a8c19c492f4698393fc0a5bbb47a29a8
Edward1217 <m932012@gmail.com>
2024-07-01 18:12:26 -0700

    client change

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ export default function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
