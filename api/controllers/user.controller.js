4e81a834ff101cdf8ea67bdff8194d7bc6ce89f9
Edward1217 <m932012@gmail.com>
2024-07-19 02:03:45 -0700

    complete update listing functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -52,17 +52,6 @@ export const deleteUser = async (req, res, next) => {
   }
 };
 
-// export const getUserListings = async (req, res, next) => {
-//   if (req.user.id === req.params.id) {
-//     try {
-//       const listings = await Listing.find({userRef:req.params.id});
-//       res.status(200).json(listings)
-//     } catch(error) {
-//       next(error)
-//     }
-//   }
-// };
-
 export const getUserListings = async (req, res, next) => {
   if (req.user.id === req.params.id) {
     try {
@@ -75,3 +64,4 @@ export const getUserListings = async (req, res, next) => {
     return next(errorHandler(401, 'You can only view your own listings!'));
   }
 };
+
