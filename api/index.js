b8285dc55dbaeacc2ec6c7f17397dea1f53e7f14
Edward1217 <m932012@gmail.com>
2024-07-23 15:11:02 -0700

    commit for deploy 5

--- api/index.js
+++ api/index.js
@@ -61,6 +61,7 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import common from './common'; // For ES Modules
 dotenv.config();
 
 mongoose
