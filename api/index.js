08c1821b9579354e321acbbbe560080e23c80c53
Edward1217 <m932012@gmail.com>
2024-07-23 13:32:20 -0700

    commit for deploy 2

--- api/index.js
+++ api/index.js
@@ -60,7 +60,9 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-import path from 'path';
+import cors from 'cors';
+
+
 dotenv.config();
 
 mongoose
@@ -72,10 +74,11 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
+
 
 const app = express();
-
+app.use(cors());
+app.use(express.static('dist'))
 app.use(express.json());
 
 app.use(cookieParser());
@@ -89,11 +92,6 @@ app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
