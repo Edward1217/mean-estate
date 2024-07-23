ed5150d26145ba2d4f913f0b153a6101a75af389
Edward1217 <m932012@gmail.com>
2024-07-23 00:29:18 -0700

    create the listing item components and show listings

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -154,7 +154,7 @@ export default function CreateListing() {
         <form onSubmit={handleSubmit} className = 'flex flex-col sm:flex-row gap-4'>
             <div className='flex flex-col gap-4 flex-1' >
                 <input type='text' placeholder='Name' className='border p-3 rounded-lg' id='name'  required onChange={handleChange} value={formData.name}/>
-                <textarea type='text' placeholder='Description' className='border p-3 rounded-lg' id='description' maxLength='62' minLength='10' required onChange={handleChange} value={formData.description}/>
+                <textarea type='text' placeholder='Description' className='border p-3 rounded-lg' id='description'  required onChange={handleChange} value={formData.description}/>
                 <input type='text' placeholder='Address' className='border p-3 rounded-lg' id='address' required onChange={handleChange} value={formData.address}/>
                 <div className='flex gap-6 flex-wrap'>
                 <div>
--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -261,7 +261,7 @@ export default function CreateListing() {
                         
 
                     ))}
-                <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80' disabled={loading||uploading}>{loading? 'Updating':'Update Listing'}</button>
+                <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80' disabled={loading||uploading}>{loading? 'Updating...':'Update Listing'}</button>
                 {error && <p className='text-red-700 text-sm'>{error}</p>}   
             </div> 
         </form>
