7e13adabf4b38637a6b433e662b20648596ad8a3
Edward1217 <m932012@gmail.com>
2024-07-23 13:03:50 -0700

    commit for deploy 1

--- api/index.js
+++ api/index.js
@@ -1,9 +1,64 @@
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// import userRouter from './routes/user.route.js';
+// import authRouter from './routes/auth.route.js';
+// import listingRouter from './routes/listing.route.js'
+// import cookieParser from 'cookie-parser';
+// import path from 'path';
+// dotenv.config();
+
+// mongoose
+//   .connect(process.env.MONGO)
+//   .then(() => {
+//     console.log('Connected to MongoDB!');
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// const __dirname = path.resolve();
+
+// const app = express();
+// //use json as input of the server
+// app.use(express.json());
+
+// app.use(cookieParser());
+
+// app.listen(3000, ()=> {
+//     console.log("Server is running on PORT 3000!!!")
+// })
+
+// //req is request from client side, res is resend to client from server side
+// app.use('/api/user',userRouter);
+
+// app.use('/api/auth', authRouter);
+
+// app.use('/api/listing',listingRouter);
+
+// app.use(express.static(path.join(__dirname, '/client/dist')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// })
+
+// //Error-Handling Middleware for the next(error)
+// app.use((err,req,res,next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || "Internal Server Error";
+//   return res.status(statusCode).json({
+//     success:false,
+//     statusCode,
+//     message
+//   })
+// })
+
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-import listingRouter from './routes/listing.route.js'
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 dotenv.config();
@@ -17,24 +72,22 @@ mongoose
     console.log(err);
   });
 
-const __dirname = path.resolve();
+  const __dirname = path.resolve();
 
 const app = express();
-//use json as input of the server
+
 app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3000, ()=> {
-    console.log("Server is running on PORT 3000!!!")
-})
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
 
-//req is request from client side, res is resend to client from server side
-app.use('/api/user',userRouter);
-
+app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
-app.use('/api/listing',listingRouter);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
@@ -42,13 +95,12 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
-//Error-Handling Middleware for the next(error)
-app.use((err,req,res,next) => {
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+  const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
-    success:false,
+    success: false,
     statusCode,
-    message
-  })
-})
+    message,
+  });
+});
